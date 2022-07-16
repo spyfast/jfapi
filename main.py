612026ab4c4b201fc6d14e458c95d5ffcf4c2d02
Владимир <53353198+spyfast@users.noreply.github.com>
2022-07-16 09:41:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from api import check_token, create_short_link, send_to_mail
 
 
 if __name__ == "__main__":
-    check_token('45fc11ea6c887c7dffbfbe7bca3812b9')
+    check_token('')
     create_short_link('https://vk.com/id1')
-    send_to_mail('Subject', 'Body', 'heliti4070@shbiso.com')
+    send_to_mail('Subject', 'Body', 'mail')
